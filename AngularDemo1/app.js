4b6dae63a3fcc0c6106c965e10483b7c1f094390
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-10-24 17:19:57 +0530

    Day 1 IIT Jammu

---
+++ AngularDemo1/app.js
@@ -0,0 +1,38 @@
+angular.module("chatApp",[])
+.controller("todoCtrl",todoCtrl)
+
+function todoCtrl(){
+
+    var todo = this;
+    console.log("ctrl")
+    todo.tasks = [];
+
+
+    function addTask(t){
+        var obj = {}
+        obj.text = t;
+        obj.status = false;
+        obj.time = new Date();
+        if(obj.text){
+            todo.tasks.push(obj);
+        }
+        console.log(todo.tasks)
+        todo.task =""
+    }
+
+    function removeTask(index){
+        todo.tasks.splice(index,1);
+    }
+
+    function taskCompleted(index){
+        todo.tasks[index].status = true;
+        console.log(todo.tasks)
+    }
+
+    todo.addTask = addTask
+    todo.removeTask = removeTask
+    todo.taskCompleted = taskCompleted
+    
+
+}
+
