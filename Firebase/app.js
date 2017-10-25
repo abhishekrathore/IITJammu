d7d1e1e6d04fd6c4f33a4ab0de1112de4c04b947
anubhavshrimal <anubhavshrimal@gmail.com>
2017-10-25 11:33:36 +0530

    Cleverbot

--- Firebase/app.js
+++ Firebase/app.js
@@ -1,16 +1,40 @@
-angular.module("chatApp",["firebase"])
-.controller("chatCtrl",chatCtrl)
+angular.module("chatApp", ["firebase"])
+    .controller("chatCtrl", chatCtrl)
 
-function chatCtrl($firebaseArray){
+function chatCtrl($firebaseArray, $http) {
 
     var chat = this;
+    var cs; // cleverbot chat status
     var chats = firebase.database().ref('chats')
     chat.chats = $firebaseArray(chats);
 
-   function send(msg,name){
-    chat.chats.$add({text:msg,name:name})
-   }
-   chat.send = send
+    function send(msg, name) {
+        chat.chats.$add({ text: msg, name: name })
+        chat.reply(msg)
+    }
+    chat.send = send
+
+    chat.welcomeMsg = function () {
+        //Use your Cleverbot API key here
+        $http.get("https://www.cleverbot.com/getreply?key=CC53rdjHxp3VxXOci-xJzY5_Y1Q&input=Hi Cleverbot!")
+            .then(function (response) {/*success callback*/
+                console.log(response.data.output)
+                cs = response.data.cs
+                console.log(response.data.cs)
+                chat.chats.$add({ text: response.data.output, name: "Cleverbot" })
+            });
+    }
+
+    chat.reply = function (userMsg) {
+        var query = "https://www.cleverbot.com/getreply?key=CC53rdjHxp3VxXOci-xJzY5_Y1Q&input=" + userMsg + "&cs=" + cs
+        $http.get(query)
+            .then(function (response) {
+                cs = response.data.cs
+                chat.chats.$add({ text: response.data.output, name: "Cleverbot" })
+            })
+    }
+
+    chat.welcomeMsg()
 
 }
 
